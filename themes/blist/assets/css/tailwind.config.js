3c5f79f71b0f3bccb637c7103cc1c5d10d6f89ec
Cirillaa <polinaas2010@gmail.com>
2023-02-05 23:04:40 +0300

    Try to fix tailwind bug.

--- themes/blist/assets/css/tailwind.config.js
+++ themes/blist/assets/css/tailwind.config.js
@@ -89,11 +89,19 @@ module.exports = {
     },
   },
   theme: {
-    extend: {
-      screens: {
-        '3xl': '1280px'
-      }
-    }
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [require('@tailwindcss/typography')]
 };
