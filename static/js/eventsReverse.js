051f1ca0ba752652af6f3abb6fce7920cf60895a
Irina-M-lu <freelancelotta@gmail.com>
2023-03-08 11:02:22 +0100

    page style improved

---
+++ static/js/eventsReverse.js
@@ -0,0 +1,24 @@
+
+
+// async function eventsReverse() {
+//     const events = document.getElementsByClassName('events');
+// console.log(events);
+
+// const eventsReverse = JSON.parse(events).reverse();
+
+// return eventsReverse;
+
+// }
+
+// eventsReverse();
+// function eventsReverse (){
+//     newEvents = JSON.parse(events);
+//     newEvents.reverse();
+// console.log(newEvents);
+// return newEvents;
+
+// }
+
+
+// eventsReverse();
+
